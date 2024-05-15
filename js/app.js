5d29323a3c40d15890f62937d2ba0bdb2c51b454
DesG01211517 <desmongarrison2@gmail.com>
2024-05-15 17:12:43 -0400

    working on connecting javascript and testing before api

---
+++ js/app.js
@@ -0,0 +1,8 @@
+function getDetails(event) {
+  event.preventdefault();
+  console.log("details");
+}
+//$("#flightDateButton, #hotelDateButton").datepicker({
+//  format: "mm/dd/yyyy",
+//autoclose: true,
+//});
