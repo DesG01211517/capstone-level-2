e84339d2d894f1d75ffa799e0e6a9c644c7f5673
DesG01211517 <desmongarrison2@gmail.com>
2024-05-16 20:08:24 -0400

    added background image and corrected inital javascript fetch

--- js/app.js
+++ js/app.js
@@ -2,15 +2,17 @@ const BASE_URL = "https://financialmodelingprep.com/api/v3/search-name";
 
 //const companyName = event.target["companyName"].value;
 
-const url =
-  BASE_URL +
-  `?apikey=L04lob0HeqKpJmAotpHEUjTlZYDpuwan&query=${event.target["companyName"].value}`;
-
 function getDetails(event) {
   event.preventDefault();
   //console.log(event.target["companyName"].value);
+
+  const url =
+    BASE_URL +
+    `?apikey=L04lob0HeqKpJmAotpHEUjTlZYDpuwan&query=${event.target["companyName"].value}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
 }
-//$("#flightDateButton, #hotelDateButton").datepicker({
-//  format: "mm/dd/yyyy",
-//autoclose: true,
-//});
